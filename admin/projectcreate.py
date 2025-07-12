2a1d176d5d246532103dc2b497800aae2ce86e85
dkrushita <desaikrushita56@gmail.com>
2025-07-12 15:35:51 +0530

    Update: latest project changes

--- admin/projectcreate.py
+++ admin/projectcreate.py
@@ -1,16 +1,19 @@
-#!C:\Python312\python.exe
+#!C:/Python312/python.exe
 import cgi
 import cgitb
 cgitb.enable()
-import header
-print(''' <!DOCTYPE html>
+
+print("Content-Type: text/html\n")
+
+print(''' 
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Add New Project Form</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-     <link rel="stylesheet" href="style/projectcreate.css"> 
+<link rel="stylesheet" href="style/projectcreate.css">
 </head>
 <body>
 
@@ -19,24 +22,25 @@ print(''' <!DOCTYPE html>
       <i class="fas fa-building"></i> Add Project Master
     </div>
 
-    <form class="form-container" id="projectForm">
-     
+    <form class="form-container" id="projectForm" action="backend/projectcreatebackend.py" method="post" enctype="multipart/form-data">
 
       <!-- Image Uploads --> 
-      <label for="title">Project Images Upload </label><br>
+      <label for="title">Project Images Upload</label><br> <br>
       <div class="image-upload-wrapper">
-        
+     
         <label class="image-box">
-       
-          <input type="file" name="image1" accept="image/*">
+          <input type="file" name="image1" accept="image/*" onchange="previewImage(this)">
+          <img src="" class="preview-img">
           <span>Upload</span>
         </label>
         <label class="image-box">
-          <input type="file" name="image2" accept="image/*">
+          <input type="file" name="image2" accept="image/*" onchange="previewImage(this)">
+          <img src="" class="preview-img">
           <span>Upload</span>
         </label>
         <label class="image-box">
-          <input type="file" name="image3" accept="image/*">
+          <input type="file" name="image3" accept="image/*" onchange="previewImage(this)">
+          <img src="" class="preview-img">
           <span>Upload</span>
         </label>
       </div>
@@ -69,6 +73,22 @@ print(''' <!DOCTYPE html>
   </div>
 
   <script>
+    function previewImage(input) {
+      const file = input.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const previewImg = input.parentElement.querySelector('.preview-img');
+          const uploadText = input.parentElement.querySelector('span');
+          previewImg.src = e.target.result;
+          previewImg.style.display = 'block';
+          uploadText.style.display = 'none';
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+
+    // Toggle Switch for Status
     const toggleSwitch = document.getElementById("toggleSwitch");
     const switchText = document.getElementById("switchText");
     const statusInput = document.getElementById("statusInput");
@@ -83,5 +103,4 @@ print(''' <!DOCTYPE html>
 
 </body>
 </html>
-
 ''')
